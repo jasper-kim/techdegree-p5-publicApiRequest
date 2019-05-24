3fd3fb7837f4f27287a3c743ceb99b96e8f82904
Jasper Kim <yjikim0219@gmail.com>
2019-05-24 13:02:51 -0700

    Display employees's profile

--- js/scripts.js
+++ js/scripts.js
@@ -17,5 +17,30 @@ async function getEmployees(url) {
     return employeesJSON.results;     
 }
 
-console.log(getEmployees(url));
+// Generate the markup for each employee
+function displayEmployees(data) {
+    data.map(empolyee => {
+        const employeeHTML = `
+            <div class="card">
+            <div class="card-img-container">
+                <img class="card-img" src="${empolyee.picture.large}" alt="profile picture">
+            </div>
+            <div class="card-info-container">
+                <h3 id="name" class="card-name cap">${empolyee.name.first}</h3>
+                <p class="card-text">${empolyee.email}</p>
+                <p class="card-text cap">${empolyee.location.city}, ${empolyee.location.state}</p>
+            </div>
+            </div>
+        `;
+
+        galleryDiv.innerHTML += employeeHTML;
+    })
+}
+
+getEmployees(url)
+    .then(data => displayEmployees(data))
+    .catch( e => {
+        galleryDiv.innerHTML = "<h3>Somthing went wrong!</h3>";
+        console.error(e);
+    });
 

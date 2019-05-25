61e2c0440d8fd96570b92414c16e8ab91d1bd819
Jasper Kim <yjikim0219@gmail.com>
2019-05-25 15:50:05 -0700

    Update comments

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,19 @@
-// Constant variables
+/******************************************
+Treehouse Techdegree:
+FSJS project 5 - Public API Requests
+******************************************/
+
+//** Global variables **//
 const url = "https://randomuser.me/api/?results=12&nat=us";
 const galleryDiv = document.getElementById('gallery');
-const searchContainerDiv = document.querySelector('.search-container');
 const modals = [];
 
-// Generates async funtion to make fetch requests
+
+//** Async funtions **/
+/**
+ * Request a JSON object from Random User Generator API
+ * @param {string} url - Public API URL
+ */
 async function getJSON(url) {
     try{
         const response = await fetch(url);
@@ -14,16 +23,28 @@ async function getJSON(url) {
     }
 }
 
+/**
+ * Return employee Data from JSON object
+ * @param {string} url - Public API URL
+ */
 async function getEmployees(url) {
     const employeesJSON = await getJSON(url);
     return employeesJSON.results;     
 }
 
-//Makes markup for search
+
+/** Search **/
+/**
+ * Change display property of employee element
+ * @param {array} list - arry of html div elements 
+ * @param {object} input - object of html input element
+ */
 function searchEmployee(list, input) {
     const text = input.value;
-
+    
+    //Loop through employee element
     list.map(item => {
+        //Check if the element has the input value in its name.
         if(item.querySelector('#name').textContent.includes(text)) {
             item.style.display = "";
         }else {
@@ -32,33 +53,47 @@ function searchEmployee(list, input) {
     });
 }
 
+/**
+ * Add search markup and its function
+ */
 function generateSearch() {
-    const searDiv = `
+    //Display search elements 
+    const searchContainerDiv = document.querySelector('.search-container');
+    const searchDiv = `
         <form action="#" method="get">
-        <input type="search" id="search-input" class="search-input" placeholder="Search...">
-        <input type="submit" value="&#x1F50D;" id="serach-submit" class="search-submit">
+            <input type="search" id="search-input" class="search-input" placeholder="Search...">
+            <input type="submit" value="&#x1F50D;" id="serach-submit" class="search-submit">
         </form>
-        `;
+    `;
 
-    searchContainerDiv.innerHTML = searDiv;
-
+    searchContainerDiv.innerHTML = searchDiv;
+ 
     const searchInput = document.getElementById('search-input');
-    const searchBtn = document.getElementById('serach-submit');
+    const searchForm = document.querySelector('form');
     const cardDivs = document.querySelectorAll('.card');
     const cardArray = [...cardDivs];
 
-    searchBtn.addEventListener('click', () => {
+    //When the user clicks search form
+    searchForm.addEventListener('click', () => {
+        //Find mactched employee
         searchEmployee(cardArray, searchInput);
     });
 
-    searchInput.addEventListener('input', () => {
+    //When the user types in search form
+    searchForm.addEventListener('input', () => {
+        //Find mactched employee
         searchEmployee(cardArray, searchInput);
     })
 }
 
-// Makes markup for each employee
+/** Employee **/
+/**
+ * Display 12 Employee's card and modal window
+ * @param {array} data - array of employee data
+ */
 function displayEmployees(data) {
     data.forEach((employee, index) => {
+        //Display employees
         const cardDiv = document.createElement('div');
         cardDiv.className = 'card';
 
@@ -75,10 +110,8 @@ function displayEmployees(data) {
 
         cardDiv.innerHTML = employeeHTML;
         galleryDiv.appendChild(cardDiv);
-
-        //------------------------------------------------
-        //Modal generate
-
+        
+        //Create modal window objects of employee
         const modalDiv = document.createElement('div');
         modalDiv.className = 'modal-container';
 
@@ -103,44 +136,63 @@ function displayEmployees(data) {
         `;
 
         modalDiv.innerHTML = modalHTML;
+
+        //Store modal window objects in array
         modals.push(modalDiv);
 
+        //When the user clicks a employee
         cardDiv.addEventListener('click', () => {
+            //Show modal window
             galleryDiv.appendChild(modals[index]);
+            //Activate bottuns in the modal window
             controlModal(index);
         });
     });    
 
+    //Activate search 
     generateSearch();
 }
 
-//Reformats bitrh day
+/**
+ * Reformat employee's bitrh day
+ * @param {string} text - value from employee data
+ */
 function formatBday(text) {
     const regex = /(\d{4})-(\d{2})-(\d{2}).*/;
     return text.replace(regex, '$2/$3/$1');
 }
 
-//Closes modal window
+/**
+ * Make buttons to close and toggle modal window
+ * @param {number} index - index of array of employee data
+ */
 function controlModal(index) {
     const closeBtn = document.getElementById('modal-close-btn');
     const prevBtn = document.getElementById('modal-prev');
     const nextBtn = document.getElementById('modal-next');
     const modalDiv = document.querySelector('.modal-container');
 
+    //Remove modal window when clicking close button
     closeBtn.addEventListener('click', () => {
        modalDiv.remove();
     });
 
+    //When the user click the prev button
     prevBtn.addEventListener('click', () => {
+        //Close current modal window
         modalDiv.remove();
+        //Display previous modal window
         if(index > 0) {
             galleryDiv.appendChild(modals[index-1]);
             controlModal(index-1);
         }
     });
 
+    //When the user click the next button
     nextBtn.addEventListener('click', () => {
+        //Close current modal window
         modalDiv.remove();
+        //Display next modal window
         if(index < 11) {
             galleryDiv.appendChild(modals[index+1]);
             controlModal(index+1);
